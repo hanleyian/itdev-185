cda1893d7cbe62a42c6111795dba6bf2de0736f1
Ian Hanley <42979945+hanleyian@users.noreply.github.com>
2020-03-01 21:06:54 -0800

    Add files via upload

---
+++ Genericoo/Genericoo/input.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Genericoo
+{
+    class input
+    {
+
+        public void enter() {
+
+
+            WriteLine("Welcome to the generic program. Please enter a number.");
+            string input = ReadLine();
+
+            int p = Convert.ToInt32(input);
+
+            
+            ui<int> ui = new ui<int>(p);
+            ui.welcome();
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
